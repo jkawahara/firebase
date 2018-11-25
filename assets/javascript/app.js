a4f6ceb8a54405c19b8a77ebd99c61edfc23e407
John Kawahara <kawaharajohn@gmail.com>
2018-11-24 18:25:03 -0800

    Update static HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyC531r4gMhQnG4w6y1ZfK7wfTs2diWP9b4",
+  apiKey: "AIzaSyC531r4gMhQnG4w6y1ZfK7wfTs2diWP9b4", // Insert API Key
   authDomain: "bcbc-firebase.firebaseapp.com",
   databaseURL: "https://bcbc-firebase.firebaseio.com",
   projectId: "bcbc-firebase",
