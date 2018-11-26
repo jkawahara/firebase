2c0b9324a8bfb31d484bff2d6158a3db0556c87d
John Kawahara <kawaharajohn@gmail.com>
2018-11-25 20:50:58 -0800

    Add Firebase initialization; variables; functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,93 @@
+// Ensure document is ready
+$(document).ready(function() {
 
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyC531r4gMhQnG4w6y1ZfK7wfTs2diWP9b4", // Insert API Key
-  authDomain: "bcbc-firebase.firebaseapp.com",
-  databaseURL: "https://bcbc-firebase.firebaseio.com",
-  projectId: "bcbc-firebase",
-  storageBucket: "bcbc-firebase.appspot.com",
-  messagingSenderId: "890360445286"
-};
-firebase.initializeApp(config);
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC531r4gMhQnG4w6y1ZfK7wfTs2diWP9b4", // Insert API Key
+    authDomain: "bcbc-firebase.firebaseapp.com",
+    databaseURL: "https://bcbc-firebase.firebaseio.com",
+    projectId: "bcbc-firebase",
+    storageBucket: "bcbc-firebase.appspot.com",
+    messagingSenderId: "890360445286"
+  };
+  firebase.initializeApp(config);
+
+  // Declare variable for firebase database
+  var database = firebase.database();
+
+  // FUNCTIONS
+  // =========
+
+  // Get train data from form and upload to /trainData database.ref
+  function addTrainData() {
+    event.preventDefault();
+
+    // Get train data from form
+    var trainName = $("#train-name-input").val().trim();
+    var trainDest = $("#destination-input").val().trim();
+    var trainFirstTime = $("#first-train-time-input").val().trim();
+    var trainFreq = $("#frequency-input").val().trim();
+
+    // Check if all form inputs have been provided
+    if (trainName !== "" && trainDest !== "" && trainFirstTime !== "" && trainFreq !== "") {
+      // Create local temporary object for holding train data
+      var newTrain = {
+        name: trainName,
+        dest: trainDest,
+        firstTime: trainFirstTime,
+        freq: trainFreq
+      }
+
+      // Upload train data to database
+      database.ref("/trainData").push(newTrain);
+
+      // Clear form inputs
+      $("#train-name-input").val("");
+      $("#destination-input").val("");
+      $("#first-train-time-input").val("");
+      $("#frequency-input").val("");
+    }
+  }
+
+  // MAIN CONTROLLER
+  // ===============
+
+  // Listen for form submit button
+  $("#add-train-btn").on("click", addTrainData);
+
+  // Listen for Firebase child_added
+  database.ref("/trainData").on("child_added", function(childSnapshot) {
+
+    // Locally store database snapshot
+    var trainName = childSnapshot.val().name;
+    var trainDest = childSnapshot.val().dest;
+    var trainFirstTime = childSnapshot.val().firstTime;
+    var trainFreq = childSnapshot.val().freq;
+
+    // Moment.js calls
+    // First time, pushed back 1 year to ensure before current time
+    var firstTimeConverted = moment(trainFirstTime, "HH:mm").subtract(1, "years");
+    // Difference between current time and first time
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    // Time apart
+    var tRemainder = diffTime % trainFreq;
+    // Minutes until next train
+    var tMinutesTillTrain = trainFreq - tRemainder;
+    // Next train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+
+    // Create new entries to add to current train schedule
+    var newRow = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(trainDest),
+      $("<td>").text(trainFreq),
+      $("<td>").text(moment(nextTrain).format("hh:mm A")),
+      $("<td>").text(tMinutesTillTrain),
+    );
+    $("#current-trains > tbody").append(newRow);
+
+    // Error handler
+  }, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+  });
+});
