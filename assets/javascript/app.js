f2811dd6f31c627d64179358979e522a2320c163
John Kawahara <kawaharajohn@gmail.com>
2018-11-26 22:48:58 -0800

    Add dataRefresh function; update README.md

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,9 +13,38 @@ $(document).ready(function() {
   firebase.initializeApp(config);
 
   // VARIABLES
-  // ================
+  // =========
   var database = firebase.database(); // Declare variable for firebase database
-  var newRow; // Declare variable for refreshing data every minute
+  
+  // connectionsRef references a specific location in our database.
+  // All of our connections will be stored in this directory.
+  var connectionsRef = database.ref("/connections");
+
+  // '.info/connected' is a special location provided by Firebase that is updated
+  // every time the client's connection state changes.
+  // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+  var connectedRef = database.ref(".info/connected");
+
+  // When the client's connection state changes...
+  connectedRef.on("value", function(snap) {
+
+    // If they are connected..
+    if (snap.val()) {
+
+      // Add user to the connections list.
+      var con = connectionsRef.push(true);
+      // Remove user from the connection list when they disconnect.
+      con.onDisconnect().remove();
+    }
+  });
+
+  // When first loaded or when the connections list changes...
+  connectionsRef.on("value", function(snap) {
+
+    // Display the viewer count in the html.
+    // The number of online users is the number of children in the connections list.
+    $("#connected-viewers").text(snap.numChildren());
+  });
   
   // FUNCTIONS
   // =========
@@ -51,31 +80,43 @@ $(document).ready(function() {
     }
   }
 
+  // Refresh current train schedule every minute
   function dataRefresh() {
-    console.log("timing")
-    // Moment.js calls
-    // // First time, pushed back 1 year to ensure before current time
-    // var firstTimeConverted = moment(trainFirstTime, "HH:mm").subtract(1, "years");
-    // // Difference between current time and first time
-    // var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    // // Time apart
-    // var tRemainder = diffTime % trainFreq;
-    // // Minutes until next train
-    // var tMinutesTillTrain = trainFreq - tRemainder;
-    // // Next train
-    // var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    database.ref("/trainData").on("value", function(snapshot) {
 
-    // Create new entries to add to current train schedule
-    newRow = $("<tr>").append(
-      $("<td>").text(trainName),
-      $("<td>").text(trainDest),
-      $("<td>").text(trainFreq),
-      $("<td>").text(moment(nextTrain).format("hh:mm A")),
-      $("<td>").text(tMinutesTillTrain),
-    );
-    $("#current-trains > tbody").append(newRow);
+      $("#current-trains > tbody").empty();
+
+      snapshot.forEach(function(childSnapshot) {
+        // Locally store database snapshot
+        var trainName = childSnapshot.val().name;
+        var trainDest = childSnapshot.val().dest;
+        var trainFirstTime = childSnapshot.val().firstTime;
+        var trainFreq = childSnapshot.val().freq;
+
+        // Moment.js calls
+        // First time, pushed back 1 year to ensure before current time
+        var firstTimeConverted = moment(trainFirstTime, "HH:mm").subtract(1, "years");
+        // Difference between current time and first time
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        // Time apart
+        var tRemainder = diffTime % trainFreq;
+        // Minutes until next train
+        var tMinutesTillTrain = trainFreq - tRemainder;
+        // Next train
+        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+
+        // Create new entries to add to current train schedule
+        newRow = $("<tr>").append(
+          $("<td>").text(trainName),
+          $("<td>").text(trainDest),
+          $("<td>").text(trainFreq),
+          $("<td>").text(moment(nextTrain).format("hh:mm A")),
+          $("<td>").text(tMinutesTillTrain),
+        );
+        $("#current-trains > tbody").append(newRow);
+      });
+    });
   }
-
 
   // MAIN CONTROLLER
   // ===============
@@ -85,7 +126,6 @@ $(document).ready(function() {
 
   // Listen for Firebase child_added
   database.ref("/trainData").on("child_added", function(childSnapshot) {
-
     // Locally store database snapshot
     var trainName = childSnapshot.val().name;
     var trainDest = childSnapshot.val().dest;
@@ -125,6 +165,6 @@ $(document).ready(function() {
   // ==============
 
   // Timer delay to update train data
-  setInterval(dataRefresh, 5000);
+  setInterval(dataRefresh, 60000);
 
 });
