20557e68eae7583f1b53c552d797af76c5ee3bdf
John Kawahara <kawaharajohn@gmail.com>
2018-11-29 00:01:47 -0800

    Debug Firebase authentication

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,28 @@
+// GitHub sign-in flow per Firebase JavaScript SDK
+// ===============================================
+var provider = new firebase.auth.GithubAuthProvider();
+
+// To sign in with a pop-up window, call signInWithPopup:
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+
+
 // Ensure document is ready
+// ========================
 $(document).ready(function() {
 
   // Initialize Firebase
