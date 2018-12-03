8a4063daca7dcb411ceedc902af176ff08632712
John Kawahara <kawaharajohn@gmail.com>
2018-12-02 17:24:56 -0800

    Add Firebase authentication

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,3 @@
-// GitHub sign-in flow per Firebase JavaScript SDK
-// ===============================================
-var provider = new firebase.auth.GithubAuthProvider();
-
-// To sign in with a pop-up window, call signInWithPopup:
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
-
-
 // Ensure document is ready
 // ========================
 $(document).ready(function() {
@@ -35,6 +12,7 @@ $(document).ready(function() {
     messagingSenderId: "890360445286"
   };
   firebase.initializeApp(config);
+  
 
   // VARIABLES
   // =========
